d04f7113723257dd99c1c0adb785b298b8c4ccf4
Mariya Rose <mariel.jesm@gmail.com>
2021-09-06 16:09:39 +1000

    Task Controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,28 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:update, :destroy]
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to root_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :date, :completed, :is_event, :position, :section_id)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
 end
