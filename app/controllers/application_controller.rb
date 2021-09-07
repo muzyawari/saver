2340c30100225df38ca3aead9f10f669ef2161b1
Nick Ang <nicholas_ang@outlook.com>
2021-09-07 17:32:01 +1000

    f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
     # devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :phone, :gender, :dob])
   end
 
+  # set Signout
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
