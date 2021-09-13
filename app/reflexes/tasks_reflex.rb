5258d03faa223b5871e69d61aa3e6faed447798e
Nick Ang <nicholas_ang@outlook.com>
2021-09-13 14:35:37 +1000

    fix drag

--- app/reflexes/tasks_reflex.rb
+++ app/reflexes/tasks_reflex.rb
@@ -4,7 +4,7 @@ class TasksReflex < ApplicationReflex
   def sort
     tasks = JSON.parse(element.dataset.tasks)
     tasks.each do |task|
-      task_record = Task.find(task["id"])
+      task_record = Task.find(task["task"])
       task_record.update(position: task["position"])
     end
   end
