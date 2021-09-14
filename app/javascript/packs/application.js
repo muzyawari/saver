29dd0655ab607a1c53e9531af046ba11b22b521c
Mariya Rose <mariel.jesm@gmail.com>
2021-09-14 14:05:08 +1000

    Folder duplicates removed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,8 +22,6 @@ ActiveStorage.start();
 import "bootstrap";
 
 // Internal imports, e.g:
-
-// import { startTimer } from '../components/timer.js';
 import { timer, startTimer } from "../components/timer.js";
 
 import { initFlatpickr } from "../plugins/flatpickr";
@@ -31,66 +29,29 @@ import { loadDynamicBannerText } from "../components/banner";
 import "chartkick/chart.js";
 import Chart from "chart.js";
 
-document.addEventListener("turbolinks:load", () => {
-  // Call your functions here, e.g:
 
+const checkbox = document.querySelector("#toggle");
+const monthly = document.querySelector(".monthly");
+const weekly = document.querySelector(".weekly");
+const toggleCalender = () => {
+  if (checkbox.checked && params[:week] === "true") {
+    monthly.style.display = "none";
+    weekly.style.display = "block";
+  } else {
+    weekly.style.display = "none";
+    monthly.style.display = "block";
+  }
+}
+
+document.addEventListener("turbolinks:load", () => {
   // Date Picker for the Forms
   initFlatpickr();
-
-  // Toggle Calender Views - Monthly & Weekly Calender
-  const checkbox = document.querySelector('#toggle');
-  const monthly = document.querySelector('.monthly');
-  const weekly = document.querySelector('.weekly');
-  weekly.style.display = 'none';
-  // if (!(localStorage.getItem('toggled')) && checkbox.checked) {
-  //   monthly.style.display = 'none';
-  //   weekly.style.display = 'block';
-  //   checkbox.checked = true;
-  // }
-  window.localStorage.setItem("toggled", (!localStorage.getItem('toggled')));
-
-  if (checkbox.checked) {
-    localStorage.clear();
-    window.localStorage.setItem("toggled", (!localStorage.getItem('toggled')));
-  } else {
-    localStorage.clear();
-    window.localStorage.setItem("toggled", (!localStorage.getItem('toggled')));
-  }
-
-  checkbox.addEventListener('change', () => {
-    if (checkbox.checked && localStorage.getItem('toggled')) {
-      monthly.style.display = 'none';
-      weekly.style.display = 'block';
-    } else {
-      weekly.style.display = 'none';
-      monthly.style.display = 'block';
-    }
-  });
 
   // Dynamic Text
   const homePageText = document.getElementById("banner-typed-text");
   if (homePageText) {
     loadDynamicBannerText();
   }
-
-  // Sortable for Tasks
-  const el = document.querySelector(".tasks-list");
-  if (el) {
-    const tasksortable = Sortable.create(el, {
-      animation: 150,
-      ghostClass: "ghost",
-    });
-  }
-
-
-  // Sortable for Widgets
-  const sec = document.getElementById("sections-list");
-  const sortable = Sortable.create(sec, {
-    ghostClass: "ghost",
-    swapThreshold: 0.87,
-    animation: 150,
-  });
-
 
   // Timer Default Buttons - 1 Min, 5 Mins, 15 Mins
   const buttons = document.querySelectorAll("[data-time]");
@@ -101,18 +62,9 @@ document.addEventListener("turbolinks:load", () => {
   });
 
   // Toggle Calender Views - Monthly & Weekly Calender
-  const checkbox = document.querySelector("#toggle");
-  const monthly = document.querySelector(".monthly");
-  const weekly = document.querySelector(".weekly");
-  weekly.style.display = "none";
-  checkbox.addEventListener("change", () => {
-    if (checkbox.checked) {
-      monthly.style.display = "none";
-      weekly.style.display = "block";
-    } else {
-      weekly.style.display = "none";
-      monthly.style.display = "block";
-    }
+
+    checkbox.addEventListener("change", () => {
+    toggleCalender();
   });
 
   // Timer Entry
@@ -149,11 +101,6 @@ document.addEventListener("turbolinks:load", () => {
   });
 });
 
-// Dynamic Text
-document.addEventListener("turbolinks:load", () => {
-  const homePageText = document.getElementById("banner-typed-text");
-  if (homePageText) {
-    loadDynamicBannerText();
-  }
-});
+
+
 import "controllers";
