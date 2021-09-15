a34f8f9976336ecce485fdf6fd2c8ab50a6703e8
Mariya Rose <mariel.jesm@gmail.com>
2021-09-15 12:17:59 +1000

    Changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
+    @user = current_user
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :phone, :gender, :dob])
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :phone, :gender, :dob, :bg_color, :txt_color])
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  attr_accessor :bg_color, :txt_color
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :folders, dependent: :destroy
