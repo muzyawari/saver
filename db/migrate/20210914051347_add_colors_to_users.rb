b500179518f0af29bb17ec4348efacbfeea529f5
Mariya Rose <mariel.jesm@gmail.com>
2021-09-17 11:08:12 +1000

    Colors Fixed

--- db/migrate/20210914051347_add_colors_to_users.rb
+++ db/migrate/20210914051347_add_colors_to_users.rb
@@ -1,6 +1,6 @@
 class AddColorsToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :bg_color, :string
-    add_column :users, :txt_color, :string
+    add_column :users, :bg_color, :string, default: "#151828"
+    add_column :users, :txt_color, :string, default: "#FFFFFF"
   end
 end
--- db/migrate/20210915094923_add_card_colors_to_user.rb
+++ db/migrate/20210915094923_add_card_colors_to_user.rb
@@ -1,5 +1,5 @@
 class AddCardColorsToUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :card_color, :string
+    add_column :users, :card_color, :string, default: "#372f2f"
   end
 end
